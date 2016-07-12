64d50faa027cc999148905736cab5bf0f67e31c8
Pedro Lopes <plopesresearch@gmail.com>
2016-07-12 18:09:37 +0200

    solved issue with __init__ but not tested yet

--- apps/python/tests/test_openems_module.py
+++ apps/python/tests/test_openems_module.py
@@ -1,14 +1,14 @@
 import sys
 sys.path.append("../")
 from pyEMS import openEMSstim
-from pyEMS import EMSCommand
+from pyEMS.EMSCommand import ems_command
 
 #create a ems_device
 ems_device = openEMSstim.openEMSstim("/dev/tty.usb",19200)
 if ems_device: #check if was created properly
     print("Device Found")
     print("Will send: Channel 1, Intensity=1 (out of 100), Duration=1000 (1 second)")
-    command_1 = EMSCommand.EMSCommand(1,1,1000)
+    command_1 = ems_command(1,1,1000)
     print(str(command_1))
     ems_device.send(command_1)
 
