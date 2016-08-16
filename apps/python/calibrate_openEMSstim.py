55a5a80a4a63db99da2d0f09220af9448a1a6a39
Pedro Lopes <plopesresearch@gmail.com>
2016-08-16 12:00:14 +0200

    calibration for python now repeats last command

--- apps/python/calibrate_openEMSstim.py
+++ apps/python/calibrate_openEMSstim.py
@@ -1,7 +1,7 @@
 # This program enables to send commands via USB to the openEMSstim board
 # NOTE: To use it you will need to adjust the serial_port to whatever your operating system reported for the arduino connection (Windows will be something like COM3, check on Device Manager, while unix will be /dev/tty.usbsomething)
-serial_port = "/dev/tty.wchusbserial1420"
-
+serial_port = "/dev/tty.usbserial-A9Q51NVR"
+import os
 import sys
 from time import sleep
 from pyEMS import openEMSstim
@@ -9,9 +9,12 @@ from pyEMS.EMSCommand import ems_command
 from pyEMS.channel import Channel
 from pyEMS.preset import Preset
 
+# create a new openEMSstim board
 my_ems_board = openEMSstim.openEMSstim(serial_port,19200) #adjust serial_port above, do not change baudrate if you don't know why
-not_ended = True
 
+# settings for commands
+not_ended = True
+print_channel_config_next_time = False
 command_list = sys.argv[0] + "choose of of the commands below:\n"
 command_list += "\t[1|2] [0-100] [0-2000] e.g., 1 100 1000 (stimulates on channel 1, with 100% for 1000 ms)\n"
 command_list += "\t[1|2][+|-] increases(+)/decreases(-) the intensity of this channel by 1%\n"
@@ -31,37 +34,76 @@ channels[0].add_preset(max_intensity)
 channels[0].add_preset(min_intensity)
 channels[1].add_preset(max_intensity)
 channels[1].add_preset(min_intensity)
+command_history = []
 
 def cli_clear():
-    if os.type != "Windows":
-        print(chr(27) + "[2J")
+    #if os.type != "Windows":
+    #    print(chr(27) + "[2J")
+    pass
+
+def print_configuration(print_channel_config_next_time):
+    for c in channels:
+        print("-----------")
+        print(c.name)
+        print(c.intensity)
+    print("-----------")
+    print_channel_config_next_time = False
+
+def stimulate(ems_board, command, command_history, save_command_to_history): 
+    if command:
+        if save_command_to_history:
+            command_history.append(command)
+        else:
+            command = command_history[-1]
+        print("openEMSstim: stimulates now (raw command is:" + str(command) + ")")  
+        ems_board.send(command)
+    else:
+        print("Error: command invalid, hence not sent")
 
 while (not_ended):
     cli_clear()
+    print(print_channel_config_next_time)
+    if print_channel_config_next_time:
+        print_configuration(print_channel_config_next_time)
     command = raw_input(command_list)
     command_tokens = command.split(" ")
-    if len(command_tokens) == 3: #direct command mode
-        #command_history.append(command)
-        #my_ems_board.send(ems_command(2,100,1000))
-        print("Direct command mode") 
-    elif len(command_tokens) == 2: #save preset command
+    
+    # direct command mode
+    if len(command_tokens) == 3: 
+        command = ems_command(command_tokens[0],command_tokens[1],command_tokens[2])
+        stimulate(my_ems_board, command, command_history, True)
+
+   
+    
+    # save preset mode
+    elif len(command_tokens) == 2: 
         if command_tokens[0] == "p":
             #p = Preset(command_history[-1])
             print("save preset mode")
 
-    elif len(command_tokens) == 1: #repeat command or string commands
-        if command_tokens[0] == "r":
-            print("repeat mode")
+    # repeat command mode or all single "string" commands
+    elif len(command_tokens) == 1: 
 
+        # repeat last command mode
+        if command_tokens[0] == "r":
+            if len(command_history) >= 1:
+                stimulate(my_ems_board, command, command_history, False)
+            else:
+                print("Warning: there is no last command to repeat (history empty)")
+
+        # display channel configurations mode
         elif command_tokens[0] == "d":
-            print("display mode")
-            for c in channels:
-                print(c.name)
-                print(c.intensity)
+            print_channel_config_next_time = True  
+
+        # save preset mode
         elif command_tokens[0] == "s":
             print("save mode")
+
+        # quit program
         elif command_tokens[0] == "q":
             not_ended = False
+
+        # repeat command with de/in-crease in amplitude
         elif len(command_tokens[0]) == 2: #does this work?
             print("repeat command with +-")
 
--- apps/python/pyEMS/EMSCommand.py
+++ apps/python/pyEMS/EMSCommand.py
@@ -5,6 +5,10 @@
 version = '0.1'
 
 def ems_command(channel, intensity, duration):
+    if (not channel.isdigit()) or (not intensity.isdigit()) or (not duration.isdigit()):
+        print("Malformatted input " + str(channel) + " " + str(intensity) + " " + str(duration))
+        return None
+    
     # forcing everything to be an int
     channel = int(channel)
     intensity = int(intensity)
@@ -15,20 +19,20 @@ def ems_command(channel, intensity, duration):
     if channel == 1 or channel == 2:
         command += str("C" + str(channel-1))
     else:
-        print("Malformatted input at channel number:" + str(channel))
+        print("Malformatted input at channel number:" + str(channel) + ". Your device supports only channels 1 or 2")
         return None
             
     # check intensity input validity
     if intensity>= 0 and intensity<= 100:
         command += str("I"+ str(intensity))
     else:
-        print("Malformatted input at intensity number:" + str(intensity))
+        print("Malformatted input at intensity number:" + str(intensity) + ". Intensity must be betwen 0-100 (integer)" )
         return None 
     
     # check duration input validity
     if duration >= 0:
         command += str("T"+ str(duration) + "G")
     else:
-        print("Malformatted input at duration number:" + str(duration))
+        print("Malformatted input at duration number:" + str(duration) + ". Duration in milliseconds must be a positive integer, e.g., 1000")
         return None
     return command
