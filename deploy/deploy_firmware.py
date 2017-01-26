44d2802cfd29f91813cf214f8ef48a6bae9728d1
Pedro Lopes <plopesresearch@gmail.com>
2017-01-26 17:30:41 +0100

    working on deploy script

--- deploy/deploy_firmware.py
+++ deploy/deploy_firmware.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append('../apps/python/')
 from pyEMS import openEMSstim
 from pyEMS.EMSCommand import ems_command
+from glob import glob
 
 #setting the deploy environment
 testing = True
@@ -22,6 +23,11 @@ def deployOnBoard():
     deploy = subprocess.call(["ino","upload"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print(deploy)
 
+def listdir():
+    lsdir = subprocess.Popen(["ls", "-la"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    results, err = lsdir.communicate()
+    print(results)
+
 def setupInoDevEnv():
     print("TODO: still need setup the ino env automatically w/ latest build and also double checks git pull")
     #ask for git pull?
@@ -29,16 +35,29 @@ def setupInoDevEnv():
     if gitpull == 'y':
         git = subprocess.call(["git","pull"])
         print(git)
-    mv = subprocess.call(["mv",sys.argv[0]]+" ../")
+    print(sys.argv[0])
+    mv = subprocess.call(["mv",str(sys.argv[0]),".."])
     print(mv)
+    listdir()
+    rm = raw_input("Will delete all files (as above) in this dir (except this script). OK? [y/n]")
+    if rm == 'y':
+        files_to_delete = glob("*")
+        for file_rm in files_to_delete:
+            rm = subprocess.call(["rm","-rf", file_rm])
+        #rm = subprocess.call(["rm","-r", ".build"])
+        print(rm)
     init = subprocess.call(["ino","init"])
     print(init)
+    rm = subprocess.call(["rm","src/sketch.ino"])
+    print(rm)
     #make ino.ini file without board serial and upload but with build atmega type
-    mv = subprocess.call(["mv",sys.argv[0]]+" ../")
+    mv = subprocess.call(["mv","../"+str(sys.argv[0]),"."])
     print(mv)
-    cp = subprocess.call(["cp","../arduino-openEMSstim/* src/")
-    print(cp)
-    #ready to go
+    files_to_copy = glob("../arduino-openEMSstim/*")
+    for file_cp in files_to_copy:
+        cp = subprocess.call(["cp","../arduino-openEMSstim/" + file_cp, "src/"])
+        print(cp)
+    print("STATUS: build environment ready")
 
 def find_arduino_port():
     find_usb = subprocess.Popen(["ls","/dev/"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -49,11 +68,11 @@ def find_arduino_port():
     for a in search_results:
         if a.find("tty.") > -1: 
             if a.find("tty.usb") > -1: 
-                print("Found arduino with FTDI driver")
+                print("STATUS: Found arduino with FTDI driver")
                 arduino_port = "/dev/" + str(a)
                 return a, search_results
             elif a.find("tty.wchusb") > -1: 
-                print("Found arduino with non-FTDI driver")
+                print("STATUS: Found arduino with non-FTDI driver")
                 arduino_port = "/dev/" + str(a)
                 return a, search_results
     return None, None
@@ -65,57 +84,57 @@ def change_line_to(filename,line, text):
     with open(filename, 'w') as file:
         file.writelines( data )
 
-print("openEMSstim Firmware loader for multiple boards.")
-board_no = int(raw_input("How many boards are we loading? "))
-curr_id = int(raw_input("What is the first ID number? "))
+print("MENU: openEMSstim Firmware loader for multiple boards.")
+board_no = int(raw_input("MENU: How many boards are we loading? "))
+curr_id = int(raw_input("MENU: What is the first ID number? "))
 setupInoDevEnv()
 while board_no >= 0:
-    print("Ready.")
-    print("Connect the first board now.")
+    print("STATUS: Ready.")
+    print("ACTION: Connect the first board now.")
     arduino_port = True
     while arduino_port is None:
         arduino_port = find_arduino_port()
         arduino_port, search_results = find_arduino_port()
         print arduino_port
         if arduino_port is None:
-            print("Could not find a arduino, here is what we found:") 
+            print("STATUS: Could not find a arduino, here is what we found:") 
             print(search_results)
             choice = raw_input("Try again? (enter to continue, q to exit)")
             if choice == 'q':
                 exit(0)
-    print("Board pluged to " + str(arduino_port))
-    ready = raw_input("Confirm? (type enter to continue)")
+    print("STATUS: Board pluged to " + str(arduino_port))
+    ready = raw_input("ACTION: Confirm? (type enter to continue)")
     if ready == "":
         print("BUILDING: software to include ID:" + str(curr_id))
         change_line_to(code_filename, 20, text+"\"emsB"+str(curr_id)+"\"")
-        print("UPLOAD: to board with ID:" + str(curr_id))
+        print("UPLOADING: to board with ID:" + str(curr_id))
         #deployOnBoard()        
     if testing == True:
-        print("TEST: executing python test on board ID: " + str(curr_id))
+        print("TESTING: executing python test on board ID: " + str(curr_id))
             
         #create a ems_device
         ems_device = openEMSstim.openEMSstim(arduino_port,19200)
         if ems_device: #check if was created properly
-            print("Device Found")
-            print("Will send: Channel 1, Intensity=1 (out of 100), Duration=1000 (1 second)")
+            print("STATUS: Device Found")
+            print("TEST: stimulation to Channel 1, Intensity=1 (out of 100), Duration=1000 (1 second)")
             command_1 = ems_command(1,1,1000)
             ems_device.send(command_1)
             wait = None
             while wait == 'y' or wait == 'n':
-                wait = raw_input("Did you see the LED on this channel go ON? [y/n/r for repeat]")
+                wait = raw_input("ACTION: Did you see the LED on this channel go ON? [y/n/r for repeat]")
                 if wait == 'r':
                     ems_device.send(command_1)
                 elif wait == 'y':
                     print("TEST/LOG/B"+str(curr_id)+": LED1 OK") #should change to log
                 elif wait == 'n':
                     print("TEST/LOG/B"+str(curr_id)+": LED1 FAIL") #should change to log
-            print("Now will open channel at maximum (100) and you will open the EMS slowly for that channel")
+            print("TEST: stimulation to channel 1 at maximum (100) and you will open the EMS slowly for that channel")
             #send direct writes using short commands
             #command_1 = ems_command(1,1000,2000)
             #ems_device.send(command_1)
             wait = None
             while wait == 'y' or wait == 'n':
-                wait = raw_input("Did you feel the EMS on this channel go ON? [y/n/r for repeat]")
+                wait = raw_input("ACTION: Did you feel the EMS on channel 1? [y/n/r for repeat]")
                 if wait == 'r':
                     pass
                     #ems_device.send(command_1)
@@ -123,12 +142,12 @@ while board_no >= 0:
                     print("TEST/LOG/B"+str(curr_id)+": EMS1 OK") #should change to log
                 elif wait == 'n':
                     print("TEST/LOG/B"+str(curr_id)+": EMS1 FAIL") #should change to log
-            print("Will send: Channel 2, Intensity=1 (out of 100), Duration=1000 (1 second)")
+            print("TEST: stimulation to Channel 2, Intensity=1 (out of 100), Duration=1000 (1 second)")
             command_2 = ems_command(2,1,1000)
             ems_device.send(command_2)
             wait = None
             while wait == 'y' or wait == 'n':
-                wait = raw_input("Did you see the LED on this channel go ON? [y/n/r for repeat]")
+                wait = raw_input("ACTION: Did you see the LED on this channel go ON? [y/n/r for repeat]")
                 if wait == 'r':
                     ems_device.send(command_2)
                 elif wait == 'y':
@@ -139,7 +158,7 @@ while board_no >= 0:
             #do EMS for channel 2
             
             #now do both channels
-            print("Will send: Channel 1 + 2 (two commands) with Intensity=40 (out of 100), Duration=2000 (2 seconds)")
+            print("TEST: stimulation to Channel 1 + 2 (two commands) with Intensity=40 (out of 100), Duration=2000 (2 seconds)")
             command_1 = ems_command(1,40,2000)
             command_2 = ems_command(2,40,2000)
             ems_device.send(command_1)
@@ -147,7 +166,6 @@ while board_no >= 0:
             # do wait and ask
             ems_device.shutdown()
             print("TEST:Done.")
+            #write test results
     curr_id += 1
     board_no-=1
-
-# also outputs a log file with time and stamps of when each arduino got logged and maybe some info?
